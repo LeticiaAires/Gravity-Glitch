dccee4400729ebc7ec84642b15cf88f6a8bd18a7
Solène <128179278+Salom0701@users.noreply.github.com>
2024-01-23 11:22:38 +0100

    Update Game.py

--- Game_classes/Game.py
+++ Game_classes/Game.py
@@ -1,15 +1,13 @@
 import os
-import time
 import pygame
 import random
-import math, os
+import math
 from random import randint
 from collections import deque
 from pygame.locals import *
 import sys
 
-from MenuManager import MenuManager
-from PauseWindow import PauseWindow
+from personnalizedBird import personnalisedBird
 
 rnd = random.Random()
 
@@ -20,57 +18,6 @@ ANIMATION_SPEED = 0.18
 WIN_WIDTH = 284 * 2
 WIN_HEIGHT = 512
 
-
-class Bird(pygame.sprite.Sprite):
-
-    WIDTH = HEIGHT = 32
-    SINK_SPEED = 0.18
-    CLIMB_SPEED = 0.3
-    CLIMB_DURATION = 333.3
-
-    def __init__(self, x, y, msec_to_climb, images):
-
-        super(Bird, self).__init__()
-
-        self.x, self.y = x, y
-        self.msec_to_climb = msec_to_climb
-        self._img_wingup, self._img_wingdown = images
-        self._mask_wingup = pygame.mask.from_surface(self._img_wingup)
-        self._mask_wingdown = pygame.mask.from_surface(self._img_wingdown)
-
-    def update(self, delta_frames=1):
-
-        if self.msec_to_climb > 0:
-
-            frac_climb_done = 1 - self.msec_to_climb/Bird.CLIMB_DURATION
-            self.y -= (Bird.CLIMB_SPEED * frames_to_msec(delta_frames) *
-                       (1 - math.cos(frac_climb_done * math.pi)))
-            self.msec_to_climb -= frames_to_msec(delta_frames)
-
-        else:
-
-            self.y += Bird.SINK_SPEED * frames_to_msec(delta_frames)
-
-    @property
-
-    def image(self):
-        return self._img_wingup if pygame.time.get_ticks() % 500 >= 250 else self._img_wingdown
-
-
-
-
-    @property
-
-    def mask(self):
-        return self._mask_wingup if pygame.time.get_ticks() % 500 >= 250 else self._mask_wingdown
-
-
-
-
-    @property
-
-    def rect(self):
-        return Rect(self.x, self.y, Bird.WIDTH, Bird.HEIGHT)
 
 class PipePair(pygame.sprite.Sprite):
 
@@ -85,8 +32,8 @@ class PipePair(pygame.sprite.Sprite):
         self.image = pygame.Surface((PipePair.WIDTH, WIN_HEIGHT), SRCALPHA)
         self.image.convert()
         self.image.fill((0, 0, 0, 0))
-        total_pipe_body_pieces = int((WIN_HEIGHT - 3 * Bird.HEIGHT - 3 * PipePair.PIECE_HEIGHT) /
-                                     PipePair.PIECE_HEIGHT)
+        #total_pipe_body_pieces = int((WIN_HEIGHT - 3 * Bird.HEIGHT - 3 * PipePair.PIECE_HEIGHT) / PipePair.PIECE_HEIGHT)
+        total_pipe_body_pieces = int((WIN_HEIGHT - 3 * personnalisedBird.HEIGHT - 3 * PipePair.PIECE_HEIGHT) / PipePair.PIECE_HEIGHT)
         self.bottom_pieces = randint(1, total_pipe_body_pieces)
         self.top_pieces = total_pipe_body_pieces - self.bottom_pieces
 
@@ -158,12 +105,11 @@ def load_images():
         img.convert()
         return img
 
-    return {'background': load_image('background.png'),
+    return {'background': load_image('background.jpg'),
             'pipe-end': load_image('pipe_end.png'),
             'pipe-body': load_image('pipe_body.png'),
             'bird-wingup': load_image('bird_wing_up.png'),
-            'bird-wingdown': load_image('bird_wing_down.png'),
-            'gameover': load_image('gameover.png')}
+            'bird-wingdown': load_image('bird_wing_down.png')}
 
 def frames_to_msec(frames, fps=FPS):
     return 1000.0 * frames / fps
@@ -171,95 +117,67 @@ def frames_to_msec(frames, fps=FPS):
 def msec_to_frames(milliseconds, fps=FPS):
     return fps * milliseconds / 1000.0
 
-def game_over_screen(display_surface, score, font_path):
-        font = pygame.font.Font(font_path, 48)
-        button_font = pygame.font.Font(font_path, 36)
-
-        game_over_text = font.render("Game Over!", True, (255, 255, 255))
-        score_text = font.render(f"Score: {score}", True, (255, 255, 255))
-
-        images = load_images()
-        display_surface.blit(images['background'], (WIN_WIDTH, WIN_HEIGHT // 2))
-
-        quit_button = pygame.Rect(WIN_WIDTH // 2 - 75, WIN_HEIGHT // 2, 150, 50)
-        reset_button = pygame.Rect(WIN_WIDTH // 2 - 75, WIN_HEIGHT // 2 + 60, 150, 50)
-        menu_button = pygame.Rect(WIN_WIDTH // 2 - 75, WIN_HEIGHT // 2 + 120, 150, 50)
-        
-        
-        
-        display_surface.blit(game_over_text, (WIN_WIDTH // 2 - game_over_text.get_width() // 2, WIN_HEIGHT // 4))
-        display_surface.blit(score_text, (WIN_WIDTH // 2 - score_text.get_width() // 2, WIN_HEIGHT // 4 + 60))
-
-        quit_text = button_font.render("Quit", True, (0, 0, 0))
-        reset_text = button_font.render("Reset", True, (0, 0, 0))
-        menu_text = button_font.render("Menu", True, (0, 0, 0))
-        
-        display_surface.blit(quit_text, (quit_button.x + 25, quit_button.y + 15))
-        display_surface.blit(reset_text, (reset_button.x + 25, reset_button.y + 15))
-        display_surface.blit(menu_text, (menu_button.x + 25, menu_button.y + 15))
-        
-        pygame.display.flip()
-        
-        while True:
-            for event in pygame.event.get():
-                if event.type == QUIT:
+def game_over_screen(display_surface, score):
+    font = pygame.font.SysFont(None, 48, bold=True)
+    game_over_text = font.render("Game Over!", True, (255, 0, 0))
+    score_text = font.render(f"Score: {score}", True, (255, 255, 255))
+    
+    quit_button = pygame.Rect(WIN_WIDTH // 2 - 75, WIN_HEIGHT // 2, 150, 50)
+    reset_button = pygame.Rect(WIN_WIDTH // 2 - 75, WIN_HEIGHT // 2 + 60, 150, 50)
+    menu_button = pygame.Rect(WIN_WIDTH // 2 - 75, WIN_HEIGHT // 2 + 120, 150, 50)
+    
+    pygame.draw.rect(display_surface, (255, 0, 0), quit_button)
+    pygame.draw.rect(display_surface, (255, 0, 0), reset_button)
+    pygame.draw.rect(display_surface, (255, 0, 0), menu_button)
+    
+    display_surface.blit(game_over_text, (WIN_WIDTH // 2 - game_over_text.get_width() // 2, WIN_HEIGHT // 4))
+    display_surface.blit(score_text, (WIN_WIDTH // 2 - score_text.get_width() // 2, WIN_HEIGHT // 4 + 60))
+    
+    
+    quit_text = font.render("Quit", True, (255, 255, 255))
+    reset_text = font.render("Reset", True, (255, 255, 255))
+    menu_text = font.render("Menu", True, (255, 255, 255))
+    
+    display_surface.blit(quit_text, (quit_button.x + 25, quit_button.y + 15))
+    display_surface.blit(reset_text, (reset_button.x + 25, reset_button.y + 15))
+    display_surface.blit(menu_text, (menu_button.x + 25, menu_button.y + 15))
+    
+    pygame.display.flip()
+    
+    while True:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+            elif event.type == MOUSEBUTTONDOWN:
+                if quit_button.collidepoint(event.pos):
                     pygame.quit()
                     sys.exit()
-                elif event.type == MOUSEBUTTONDOWN:
-                    if quit_button.collidepoint(event.pos):
-                        pygame.quit()
-                        sys.exit()
-                    elif reset_button.collidepoint(event.pos):
-                        return "reset"
-                    elif menu_button.collidepoint(event.pos):
-                        return "menu"
+                elif reset_button.collidepoint(event.pos):
+                    return "reset"
+                elif menu_button.collidepoint(event.pos):
+                    return "menu"
 
 class Start:
-    from MenuManager import MenuManager
-    def __init__(self):
-        self.run_game()
+    def __init__(self, perso_color, prop_image, x_prop, y_prop):
+        self.run_game(perso_color, prop_image, x_prop, y_prop)
 
-    def show_intro_screen(self):
-        pygame.init()
-
-        display_surface = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
-        pygame.display.set_caption('Pygame Flappy Bird')
-        images = load_images()
-
-        intro_font = pygame.font.Font(MenuManager.font_path, 30)
-        intro_text = intro_font.render("Press SPACE to Start", True, (255, 255, 255))
-        intro_text_rect = intro_text.get_rect(center=(WIN_WIDTH / 2, WIN_HEIGHT / 2))
-
-        bird = Bird(50, int(WIN_HEIGHT / 2 - Bird.HEIGHT / 2), 0, (images['bird-wingup'], images['bird-wingdown']))
-
-        while True:
-            for x in (0, WIN_WIDTH / 2):
-                display_surface.blit(images['background'], (x, 0))
-            display_surface.blit(bird.image, bird.rect)
-            display_surface.blit(intro_text, intro_text_rect)
-
-            for event in pygame.event.get():
-                if event.type == QUIT:
-                    pygame.quit()
-                    sys.exit()
-                elif event.type == KEYUP and event.key == K_SPACE:
-                    return
-
-            pygame.display.flip()
-
-    def run_game(self):
+    def run_game(self, perso_color, prop_image, x_prop, y_prop):
         from MainMenu import MainMenu
         pygame.init()
 
-        display_surface = pygame.display.set_mode((WIN_WIDTH*2, WIN_HEIGHT))
+        display_surface = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
         pygame.display.set_caption('Pygame Flappy Bird')
         clock = pygame.time.Clock()
         score_font = pygame.font.SysFont(None, 32, bold=True)
         images = load_images()
 
-        self.show_intro_screen()
+        LunettesDeSoleil = pygame.image.load("Assets/lunettes-de-soleil.jpg").convert_alpha()
+        LunettesDeSoleil = pygame.transform.scale(LunettesDeSoleil, (40,40))
+        LunettesDeSoleil.set_colorkey((253, 253, 253))
 
-        bird = Bird(50, int(WIN_HEIGHT / 2 - Bird.HEIGHT / 2), 2, (images['bird-wingup'], images['bird-wingdown']))
+        
+        bird = personnalisedBird(50, int(WIN_HEIGHT/2 - personnalisedBird.HEIGHT/2), 2, (images['bird-wingup'], images['bird-wingdown']),perso_color,prop_image,x_prop,y_prop)
         pipes = deque()
         frame_clock = 0
         score = 0
@@ -279,27 +197,31 @@ class Start:
                 elif e.type == KEYUP and e.key in (K_PAUSE, K_p):
                     paused = not paused
                 elif e.type == MOUSEBUTTONUP or (e.type == KEYUP and e.key in (K_UP, K_RETURN, K_SPACE)):
-                    bird.msec_to_climb = Bird.CLIMB_DURATION
+                    #bird.msec_to_climb = Bird.CLIMB_DURATION
+                    bird.msec_to_climb = personnalisedBird.CLIMB_DURATION
+
 
             if paused:
                 continue
 
             pipe_collision = any(p.collides_with(bird) for p in pipes)
 
-            if pipe_collision or 0 >= bird.y or bird.y >= WIN_HEIGHT - Bird.HEIGHT:
-                result = game_over_screen(display_surface, score, "Assets/your_font.ttf")
+            #if pipe_collision or 0 >= bird.y or bird.y >= WIN_HEIGHT - personnalisedBird.HEIGHT:
+            if pipe_collision or 0 >= bird.y or bird.y >= WIN_HEIGHT - personnalisedBird.HEIGHT:
+                result = game_over_screen(display_surface, score)
                 if result == "reset":
                     # Reset the game
-                    bird = Bird(50, int(WIN_HEIGHT / 2 - Bird.HEIGHT / 2),
-                                2, (images['bird-wingup'], images['bird-wingdown']))
+                    #bird = Bird(50, int(WIN_HEIGHT/2 - Bird.HEIGHT/2), 2, (images['bird-wingup'], images['bird-wingdown']))
+                    bird = personnalisedBird(50, int(WIN_HEIGHT/2 - personnalisedBird.HEIGHT/2), 2, (images['bird-wingup'], images['bird-wingdown']),perso_color,prop_image,x_prop,y_prop)
                     pipes.clear()
                     frame_clock = 0
                     score = 0
                     paused = False
-                    self.show_intro_screen()
                 elif result == "menu":
                     MainMenu().run()
                     return MainMenu().run()
+                #self.fenetre.blit(self.fond, (0, 0))
+                    
                 else:
                     done = True
 
@@ -322,7 +244,7 @@ class Start:
                     p.score_counted = True
 
             score_surface = score_font.render(str(score), True, (255, 255, 255))
-            score_x = WIN_WIDTH / 2 - score_surface.get_width() / 2
+            score_x = WIN_WIDTH/2 - score_surface.get_width()/2
             display_surface.blit(score_surface, (score_x, PipePair.PIECE_HEIGHT))
 
             pygame.display.flip()
@@ -331,8 +253,6 @@ class Start:
 
         print('Game over! Score: %i' % score)
         pygame.quit()
-
-
 
 if __name__ == '__main__':
     Start()
